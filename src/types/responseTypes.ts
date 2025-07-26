6292d8028db4a4f6334cc2c06fcbfc3f41a85adf
KHY90 <power4206@gmail.com>
2025-07-26 18:28:54 +0900

    스키마 수정

--- src/types/responseTypes.ts
+++ src/types/responseTypes.ts
@@ -20,20 +20,35 @@ export interface Notice {
 export interface Project {
   projectId: number;
   projectTitle: string;
+  projectSummary: string;
   projectContent: string;
-  projectCreatedAt: string;
-  projectStatus: string;
   projectLink: string;
+  projectCreatedAt: string;
+  projectUpdatedAt: string;
+  projectDeletedAt: string;
+  projectFinishedAt: string;
+  projectStatus: string;
+  projectTechStack: string[];
+  projectRecruitmentRoles: string[];
+  projectUserId: number;
+  projectRegion: string;
 }
 
 // 스터디 그룹 정보 타입
 export interface StudyGroup {
   studyGroupId: number;
+  studyGroupCategory: string[];
   studyGroupTitle: string;
+  studyGroupSummary: string;
   studyGroupContent: string;
   studyGroupCreatedAt: string;
+  studyGroupUpdatedAt: string;
+  studyGroupDeletedAt: string;
+  studyGroupFinishedAt: string;
   studyGroupStatus: string;
+  studyGroupMembers: string[];
   studyGroupUserId: number;
+  studyGroupRole: string;
 }
 
 // 질문 정보 타입
